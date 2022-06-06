37fea2aa908c2259ae5627344ec3e47ab294357e
Shiva Shrestha <Shivashrestha927@gmail.com>
2022-06-06 21:44:33 +0545

    GET PUGED

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const adminData = require('./admin');
 router.get('/',(req, res, next) => {
     const products = adminData.products;
-    res.render('shop', {prods: products, docTitle:'Shop'});
+    res.render('shop', {prods: products, pageTitle:'Shop', path: '/'});
 });
 
 module.exports = router;
