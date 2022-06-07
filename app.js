de3c798bc43b1846133e382a96d478f0b7ca97d3
Shiva Shrestha <Shivashrestha927@gmail.com>
2022-06-07 14:21:59 +0545

    EJS added

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const http = require('http');
 
 const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'htmls');
+
+const app = express();
+//For Handle bars
+// const hbs  = require('express-handlebars');
+// app.engine('handlebars', hbs({extname:'hbs', defaultLayout:'layout', layoutsDir:__dirname + '/htmls'}));
+// app.set('view engine', 'handlebars');
+// app.set('views', path.join(__dirname,'htmls'));
+
+//For PUG
+// app.set('view engine', 'pug');
+// app.set('views', 'htmls');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
