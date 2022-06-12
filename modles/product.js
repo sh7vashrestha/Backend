73eedddb627652d3369bef74528b1eadbc27351d
Shiva Shrestha <Shivashrestha927@gmail.com>
2022-06-12 22:27:36 +0545

    Module

--- modles/product.js
+++ modles/product.js
@@ -1,10 +1,27 @@
-const products = [];
+const { json } = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
 module.exports =  class Product{
     constructor(t){
         this.title = t;
     }
     save() {
-        products.push(this);
+       const p =  path.join(
+        path.dirname(process.mainModule.filename),
+        'datas',
+        'product.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            let products =[];
+            if(!err){
+                products= JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err =>{
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll() {
