b39e0853a4d00f37adbf01994ac74f3566caefb0
Shiva Shrestha <Shivashrestha927@gmail.com>
2022-06-13 23:20:12 +0545

    Reducing the file

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const Product = require('../modles/product');
+
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {pageTitle: "Add-Product",
     path: '/admin/add-product',
