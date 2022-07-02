3a0d3786ef151ceca2463864a1c9c829b6f1adff
Shiva Shrestha <Shivashrestha927@gmail.com>
2022-07-02 18:03:11 +0545

    MVC

--- controllers/Shop.js
+++ controllers/Shop.js
@@ -29,4 +29,10 @@ exports.getCart = (req, res, next) => {
             path: '/cart',
             });
 };
+exports.getOrders = (req, res, next) => {
+        res.render('shop/orders', { 
+            pageTitle:'Orders',
+            path: '/orders',
+            });
+};
 
